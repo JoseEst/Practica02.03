94a956dd48f0ed1ae813e3547e420c89fa0fe78b
johne <johne@URIEL-PC>
2017-12-10 17:56:46 -0500

    Ventana Principal

--- src/practica003/controlador/EventoPrincipal.java
+++ src/practica003/controlador/EventoPrincipal.java
@@ -5,10 +5,50 @@
  */
 package practica003.controlador;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import practica003.vista.VentanaPrincipal;
+
 /**
  *
  * @author Jose
  */
-public class EventoPrincipal {
+public class EventoPrincipal implements ActionListener
+{
+    private VentanaPrincipal VentPri;
+
+    public EventoPrincipal(VentanaPrincipal VentPri) {
+        this.VentPri = VentPri;
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) 
+    {
+       if (e.getSource().equals(this.VentPri.getMenuItemList().get(0))) 
+        {
+            System.err.println("Agregar Ventana Candidato"); 
+          
+        }
+        
+        if (e.getSource().equals(this.VentPri.getMenuItemList().get(1))) 
+        {
+            System.err.println("Agregar Ventana Eleccion"); 
+          
+        }
+        if (e.getSource().equals(this.VentPri.getMenuItemList().get(2))) 
+        {
+             System.err.println("Agregar Ventana Resultado"); 
+          
+        }
+    }
+
+    public VentanaPrincipal getVentPri() {
+        return VentPri;
+    }
+
+    public void setVentPri(VentanaPrincipal VentPri) {
+        this.VentPri = VentPri;
+    }
+    
     
 }
